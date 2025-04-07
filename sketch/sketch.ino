919c19fe1e37388363009ae3b21ea566ccbb6f2b
Dzalex <milosdzaleta@live.com>
2025-04-08 00:02:16 +0200

    Refactor formating

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,18 +1,17 @@
-#include <max6675.h>
 #include <ModbusRtu.h>
+#include <max6675.h>
 
 // data array for modbus network sharing
-uint16_t au16data[16] = {
-  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1 };
+uint16_t au16data[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1 };
 
 /**
  *  Modbus object declaration
  *  u8id : node id = 0 for master, = 1..247 for slave
  *  u8serno : serial port (use 0 for Serial)
- *  u8txenpin : 0 for RS-232 and USB-FTDI 
+ *  u8txenpin : 0 for RS-232 and USB-FTDI
  *               or any pin number > 1 for RS-485
  */
-Modbus slave(1,Serial,0); // this is slave @1 and RS-232 or USB-FTDI
+Modbus slave(1, Serial, 0);  // this is slave @1 and RS-232 or USB-FTDI
 
 int thermoDO = 4;
 int thermoCS = 5;
@@ -20,25 +19,24 @@ int thermoCLK = 6;
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
-int relay = 9;  
-  
+int relay = 9;
+
 void setup() {
-  Serial.begin( 19200, SERIAL_8E1 ); // 19200 baud, 8-bits, even, 1-bit stop
+  Serial.begin(19200, SERIAL_8E1);  // 19200 baud, 8-bits, even, 1-bit stop
   slave.start();
-  // use Arduino pins 
+  // use Arduino pins
   pinMode(relay, OUTPUT);
- delay(500);
-  
+  delay(500);
 }
 
 void loop() {
-   //write current thermocouple value
-   au16data[2] = ((uint16_t) thermocouple.readCelsius()*100);
+  // write current thermocouple value
+  au16data[2] = ((uint16_t)thermocouple.readCelsius() * 100);
 
-   //poll modbus registers
-   slave.poll( au16data, 16 );
+  // poll modbus registers
+  slave.poll(au16data, 16);
 
-   //write relay value using pwm
-   analogWrite(relay, (au16data[4]/100.0)*255);
-   delay(500);
+  // write relay value using pwm
+  analogWrite(relay, (au16data[4] / 100.0) * 255);
+  delay(500);
 }
